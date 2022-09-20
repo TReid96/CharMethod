499f5fd58b0789ba7e02bc03ea6f882798271c95
TReid96 <temarkusr@gmail.com>
2022-09-20 19:16:18 -0400

    Char

---
+++ src/javaMethods/Main.java
@@ -0,0 +1,22 @@
+package javaMethods;
+
+public class Main {
+	public static void main(String args[]) {
+		String str = "Java";
+		
+		//convert string to char array.
+		char[] ch = str.toCharArray();
+		
+		//printing the char array.
+		for(int i = 0; i<ch.length; i ++) {
+			System.out.println(ch[i]);
+			
+			
+	
+		}
+				
+				
+		
+		
+	}
+}
